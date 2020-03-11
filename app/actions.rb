ec96355ad365ea254d33e8b7ed7999e0f1aeac9b
XelaEpop <pope.alexandra@gmail.com>
2020-03-11 00:43:48 +0000

    Week 3, Day 1

--- app/actions.rb
+++ app/actions.rb
@@ -7,7 +7,7 @@ def humanized_time_ago(time_ago_in_minutes)
 end
 
 get '/' do
-  finstagram_post_shark = {
+  @finstagram_post_shark = {
     username: "sharky_j",
     avatar_url: "http://naserca.com/images/sharky_j.jpg",
     photo_url: "http://naserca.com/images/shark.jpg",
@@ -20,7 +20,7 @@ get '/' do
     }]
   }
 
-  finstagram_post_whale = {
+  @finstagram_post_whale = {
     username: "kirk_whalum",
     avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
     photo_url: "http://naserca.com/images/whale.jpg",
@@ -33,7 +33,7 @@ get '/' do
     }]
   }
 
-  finstagram_post_marlin = {
+  @finstagram_post_marlin = {
     username: "marlin_peppa",
     avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
     photo_url: "http://naserca.com/images/marlin.jpg",
@@ -46,23 +46,7 @@ get '/' do
     }]
   }
 
-[finstagram_post_shark, finstagram_post_whale, finstagram_post_marlin].to_s
+@finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
+erb(:index)
 
 end
-
-get "/fizzbuzz" do
-    my_string = ""
-for number in 1..100 do
-    if number % 3 && number % 5 == 0
-    my_string = my_string + "fizzbuzz <br/>"
-    elsif number % 5 == 0
-        my_string = my_string + "buzz <br/>"
-    elsif number % 3 == 0 
-        my_string = my_string + "fizz <br/>"
-    else my_string = my_string + "#{number} <br/>"
-    end
-end
-
-return my_string
-end
-
